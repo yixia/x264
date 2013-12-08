f243528374d5afed12f08730e1ffdf5232c251fe
Crossle Song <sxm@yixia.com>
2013-12-08 18:44:47 +0800

    Fuck android-18 use log2f

--- build_android.sh
+++ build_android.sh
@@ -9,16 +9,16 @@ fi
 SOURCE=`pwd`
 PREFIX=$SOURCE/build/android
 
-SYSROOT=$ANDROID_NDK/platforms/android-18/arch-arm
+SYSROOT=$ANDROID_NDK/platforms/android-14/arch-arm
 CROSS_PREFIX=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86_64/bin/arm-linux-androideabi-
 
 ./configure  --prefix=$PREFIX \
-  --cross-prefix=$CROSS_PREFIX \
-  --enable-pic \
-  --enable-static \
-  --disable-cli \
-  --host=arm-linux \
-  --sysroot=$SYSROOT
+	--cross-prefix=$CROSS_PREFIX \
+	--enable-pic \
+	--enable-static \
+	--disable-cli \
+	--host=arm-linux \
+	--sysroot=$SYSROOT
 
 make clean
 make -j4 install || exit 1
