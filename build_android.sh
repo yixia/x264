cad0c448868b0d52b64ec53902cfcb7837875c1c
Crossle Song <sxm@yixia.com>
2013-12-11 11:12:15 +0800

    Add support neon compile

--- build_android.sh
+++ build_android.sh
@@ -11,11 +11,16 @@ PREFIX=$SOURCE/build/android
 
 SYSROOT=$ANDROID_NDK/platforms/android-14/arch-arm
 CROSS_PREFIX=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86_64/bin/arm-linux-androideabi-
+EXTRA_CFLAGS="-march=armv7-a -mfloat-abi=softfp -mfpu=neon -D__ARM_ARCH_7__ -D__ARM_ARCH_7A__"
+EXTRA_LDFLAGS="-nostdlib"
 
 ./configure  --prefix=$PREFIX \
 	--cross-prefix=$CROSS_PREFIX \
+	--extra-cflags="$EXTRA_CFLAGS" \
+	--extra-ldflags="$EXTRA_LDFLAGS" \
 	--enable-pic \
 	--enable-static \
+	--enable-strip \
 	--disable-cli \
 	--host=arm-linux \
 	--sysroot=$SYSROOT
